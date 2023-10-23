4702f4d022d6224d218ce72c2e142173db9f90b4
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-10-23 22:47:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Router>
        <Routes>
         <Route path="/" element={ <Home/> } />
-        {/* <Route path="/signin" element={ <SignIn/> } /> */}
+        <Route path="/signin" element={ <SignIn/> } />
        </Routes>
       </Router>
     </div>
